39909722d8ebd6ef96a5fe2b95ac5d423797b8b9
Mozzers <vitaglianog@gmaikl.com>
2017-11-02 14:16:44 +0000

    Hermite function

--- assignment2/hermite.m
+++ assignment2/hermite.m
@@ -1,20 +1,52 @@
-load('DATPVC\DPVC_233');
-DAT.ind(1);
-result=[];
+% load('DATPVC\DPVC_233');
+% DAT.ind(1);
+% 
+% for j=3:length(DAT.ind)-1       %Take into account first and last case later
+%     a=(DAT.ind(j)-DAT.ind(j-1))/3;
+%     a=120;
+%     
+%     b=(DAT.ind(j+1)-DAT.ind(j))/3;
+%     b=160;
+%     
+%     ecg=DAT.ecg(DAT.ind(j)-120:DAT.ind(j)+160);
 
-for j=1:length(DAT.ind)
-    ecg=DAT.ecg(DAT.ind(5)-4:DAT.ind(5)+8);
+ecg=DAT.ecg;
+
     model=ar(ecg,4);
     A=model.A;
     poles=roots(A);
-    
     pvc=0;
-    for i=2:length(poles)
+    for i=1:length(poles)
         if norm(poles(i))>0.98
-            disp("This is a PVC");
             pvc=1;
             break;
         end
     end
-    result=[result;pvc];
+%     result(j)= pvc;
+% end
+
+% test
+right = 0;
+falsePos = 0;
+falseNeg = 0;
+for i=1:length(DAT.pvc)-1
+    if DAT.pvc(i) == result(i)
+        right = right + 1;
+    else
+        if result(i) == 0
+            falseNeg = falseNeg + 1;
+        else
+            falsePos = falsePos + 1;
+        end
+    end
 end
+rightPercentage = right / length(result) * 100;
+totalError = falsePos + falseNeg;
+
+fprintf('\r')
+fprintf('==============================================\r')
+fprintf('right %f %% \r', rightPercentage)
+fprintf('errors %i \r', totalError)
+fprintf('false positives %i \r', falsePos)
+fprintf('false negative %i \r', falseNeg)
+fprintf('==============================================\r')
