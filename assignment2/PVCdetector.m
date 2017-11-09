993a6327fa31bb4291790fded02bef5c55261d30
Legelhcs <martin.schle@gmail.com>
2017-11-09 18:21:03 +0000

    add proper PVCdetector, add AFdetector

--- assignment2/PVCdetector.m
+++ assignment2/PVCdetector.m
@@ -1,9 +1,99 @@
+%load ('ecg.dat')
+%fs = 1000;
+
 load ('DATPVC/DPVC_116.mat')
-ind = DAT.ind;
 pvc = DAT.pvc;
 ecg = DAT.ecg;
-
+ind = DAT.ind;
 fs = 250;
+
+% analyse peaks
+% lowpass filter
+order = 4;
+wc = 20;
+fc = wc / (0.5 * fs);
+[b, a] = butter(order, fc, 'low');
+e1 = filtfilt(b, a, ecg);
+
+% highpass filter
+order = 4;
+wc = 5;
+fc = wc / (0.5 * fs);
+[b, a] = butter(order, fc, 'high');
+e2 = filtfilt(b, a, e1);
+
+% differentiation
+e3 = diff(e2);
+
+% potentiation
+e4 = e3.^2;
+
+% moving average
+timeWindow = 0.2;
+N = timeWindow * fs;
+b  = (1 / N) * ones(1, N);
+a  = 1;
+e5 = filter (b, a, e4);
+
+% find R peaks
+threshold = 0.7 * mean(e5);
+pause = 0.3;
+indexPause = pause * fs;
+peaks = 0;
+index = 1;
+i = 2;
+while i<=length(e5)
+    if e5(i)==threshold || e5(i-1) < threshold && e5(i) > threshold
+        peaks(index) = i;
+        index = index + 1;
+        i = i + indexPause;
+    else
+        i = i + 1;
+    end
+end
+
+% set position
+back = 0.2;
+backIndex = back * fs;
+for i=1:length(peaks)
+    minIndex = peaks(i)-backIndex;
+    stepBack = backIndex;
+    if minIndex <= 0
+        stepBack = backIndex + minIndex-2;
+        minIndex = 1;
+    end
+    maxIndex = peaks(i)+backIndex;
+    if maxIndex > length(ecg)
+        maxIndex = length(ecg);
+    end
+    tempECG = ecg(minIndex:maxIndex);
+    [~, maxIndex] = max(tempECG);
+    peaks(i) = peaks(i) + maxIndex - stepBack-2;
+end
+
+peaks = peaks';
+
+% test
+% right = 0;
+% false = 0;
+% around = 5;
+% for i=1:length(peaks)
+%     if peaks(i)-around <= ind(i) && peaks(i)+around >= ind(i)
+%     %if peaks(i) == ind(i)
+%         right = right + 1;
+%     else
+%         false = false + 1;
+%     end
+% end
+% 
+% rightPercent = right / (right + false) * 100;
+% 
+% fprintf('==============================================\r')
+% fprintf('right %f\r', rightPercent)
+% fprintf('==============================================\r')
+
+% analyse PVC peaks
+%ind = peaks; % uncomment for use own peaks
 
 % RR regularity
 RRIntervals = zeros(length(ind)-1,1);
@@ -70,7 +160,7 @@ for i=1:length(ind)
     ind(i)=start+index; % code to reassign peak to positive values
 end
 
-myPVCHermit = zeros(length(pvc),1);
+myPVCHermit = zeros(length(ind),1);
 for j=1:length(ind) % Take into account first and last case later
     a=8;
     b=8;
@@ -109,14 +199,12 @@ for i=1:length(myPVCRR)
 end
 
 % test
-right = 0;
 falsePos = 0;
 falseNeg = 0;
 truePos = 0;
 trueNeg = 0;
-for i=1:length(pvc)
+for i=1:length(myPVC)
     if pvc(i) == myPVC(i)
-        right = right + 1;
         if myPVC(i) == 0
             trueNeg = trueNeg + 1;
         else
