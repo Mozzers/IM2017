c9bb2828ee0fc8816f6855ff3e31e2a4f332b567
Mozzers <vitaglianog@gmaikl.com>
2017-11-23 22:17:34 +0000

    Marginal edit to report, AF thresholds

--- assignment2/af_result_script.m
+++ assignment2/af_result_script.m
@@ -1,57 +1,76 @@
-f1=0.04;
-f2=0.15;
-f3=0.4;
+f1=0;
+f2=40;
+f3=124;
 w_size=20;
+tRR=0.9;
+tF=0.4;
+
 for i=1:19
     if i==8
         continue
     end
-    [se1(:,i),sp1(:,i)]=af_result_function(['DATF_renamed/afdb_file-' num2str(i) '.mat'],f1,f2,f3,w_size);
+    [se1(:,i),sp1(:,i)]=af_result_function(['DATF_renamed/afdb_file-' num2str(i) '.mat'],f1,f2,f3,w_size,tRR,tF);
     disp(['performed test 1 on patient ' num2str(i)]);
 end
 
-f1=0.4;
-f2=1.5;
-f3=4;
+se1=se1';
+sp1=sp1';
+se1(8,:)=[];
+sp1(8,:)=[];
+
+
+tF=0.5;
+
 for i=1:19
     if i==8
         continue
     end
-    [se2(:,i),sp2(:,i)]=af_result_function(['DATF_renamed/afdb_file-' num2str(i) '.mat'],f1,f2,f3,w_size);
+    [se2(:,i),sp2(:,i)]=af_result_function(['DATF_renamed/afdb_file-' num2str(i) '.mat'],f1,f2,f3,w_size,tRR,tF);
     disp(['performed test 2 on patient ' num2str(i)]);
 end
 
-f1=4;
-f2=15;
-f3=40;
+se2=se2';
+sp2=sp2';
+se2(8,:)=[];
+sp2(8,:)=[];
+
+
+tF=0.6;
+
 for i=1:19
     if i==8
         continue
     end
-    [se3(:,i),sp3(:,i)]=af_result_function(['DATF_renamed/afdb_file-' num2str(i) '.mat'],f1,f2,f3,w_size);
+    [se3(:,i),sp3(:,i)]=af_result_function(['DATF_renamed/afdb_file-' num2str(i) '.mat'],f1,f2,f3,w_size,tRR,tF);
     disp(['performed test 3 on patient ' num2str(i)]);
 end
 
-m_se(1,1)=mean(se1(1,:));
-m_se(2,1)=mean(se1(2,:));
-m_se(3,1)=mean(se1(3,:));
-m_se(1,2)=mean(se2(1,:));
-m_se(2,2)=mean(se2(2,:));
-m_se(3,2)=mean(se2(3,:));
-m_se(1,3)=mean(se3(1,:));
-m_se(2,3)=mean(se3(2,:));
-m_se(3,3)=mean(se3(3,:));
+se3=se3';
+sp3=sp3';
+se3(8,:)=[];
+sp3(8,:)=[];
 
 
-m_sp(1,1)=mean(sp1(1,:));
-m_sp(2,1)=mean(sp1(2,:));
-m_sp(3,1)=mean(sp1(3,:));
-m_sp(1,2)=mean(sp2(1,:));
-m_sp(2,2)=mean(sp2(2,:));
-m_sp(3,2)=mean(sp2(3,:));
-m_sp(1,3)=mean(sp3(1,:));
-m_sp(2,3)=mean(sp3(2,:));
-m_sp(3,3)=mean(sp3(3,:));
+m_se(1,1)=mean(se1(:,1));
+m_se(1,2)=mean(se1(:,2));
+m_se(1,3)=mean(se1(:,3));
+m_se(2,1)=mean(se2(:,1));
+m_se(2,2)=mean(se2(:,2));
+m_se(2,3)=mean(se2(:,3));
+m_se(3,1)=mean(se3(:,1));
+m_se(3,2)=mean(se3(:,2));
+m_se(3,3)=mean(se3(:,3));
+
+
+m_sp(1,1)=mean(sp1(:,1));
+m_sp(1,2)=mean(sp1(:,2));
+m_sp(1,3)=mean(sp1(:,3));
+m_sp(2,1)=mean(sp2(:,1));
+m_sp(2,2)=mean(sp2(:,2));
+m_sp(2,3)=mean(sp2(:,3));
+m_sp(3,1)=mean(sp3(:,1));
+m_sp(3,2)=mean(sp3(:,2));
+m_sp(3,3)=mean(sp3(:,3));
 
 %vertical= changing classification method
 %horizontal= changing frequency parameter0
